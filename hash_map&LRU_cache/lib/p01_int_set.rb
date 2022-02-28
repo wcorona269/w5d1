94a4968615434b3dcccf156873b4cc4d7c83b80e
wcorona269 <wcorona269@gmail.com>
2022-02-28 15:41:05 -0500

    finish phase 1

--- hash_map&LRU_cache/lib/p01_int_set.rb
+++ hash_map&LRU_cache/lib/p01_int_set.rb
@@ -59,8 +59,8 @@ class IntSet
 
   def [](num)
     self.store[num % num_buckets]
-    # optional but useful; return the bucket corresponding to `num`
   end
+  # optional but useful; return the bucket corresponding to `num`
 
   def num_buckets
     self.store.length
@@ -68,7 +68,7 @@ class IntSet
 end
 
 class ResizingIntSet
-  attr_reader :count
+  attr_accessor :store, :count
 
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
@@ -76,24 +76,38 @@ class ResizingIntSet
   end
 
   def insert(num)
+    return false if include?(num)
+    self[num] << num
+    self.count += 1
+    resize! if num_buckets < self.count
+
+    num
   end
 
   def remove(num)
+    self.count -= 1 if self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
-  def [](num)
-    # optional but useful; return the bucket corresponding to `num`
-  end
-
+  
   def num_buckets
-    @store.length
+    self.store.length
+  end
+  
+  def resize!
+    old_store = self.store
+    self.count = 0
+    self.store = Array.new(num_buckets * 2) { Array.new }
+    
+    old_store.flatten.each { |num| insert(num) }
   end
 
-  def resize!
+  def [](num)
+    self.store[num % num_buckets]
   end
 end
